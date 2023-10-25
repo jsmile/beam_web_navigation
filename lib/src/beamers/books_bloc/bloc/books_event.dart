0c6ead5081ed99ef7d01a9189f493fe86afa8642
jsmile <jsmile@naver.com>
2023-10-25 23:32:34 +0900

    Beamer - books bloc 작업중

---
+++ lib/src/beamers/books_bloc/bloc/books_event.dart
@@ -0,0 +1,4 @@
+part of 'books_bloc.dart';
+
+@immutable
+sealed class BooksEvent {}
---
+++ lib/src/beamers/books_bloc/bloc/books_state.dart
@@ -0,0 +1,6 @@
+part of 'books_bloc.dart';
+
+@immutable
+sealed class BooksState {}
+
+final class BooksInitial extends BooksState {}
